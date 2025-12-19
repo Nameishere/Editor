3fd47ad79f436dbb34475ab4c6619ece1b072de4
Nameishere <piercegeorge@outlook.com>
2025-12-19 20:33:10 +1300

    backup for trip

--- src/MadIncEditor.c
+++ src/MadIncEditor.c
@@ -40,6 +40,10 @@ enum editorKey {
     END_KEY,
 };
 
+enum editorHighlight {
+    HL_NORML = 0,
+    HL_NUMBER
+};
 /* data */
 
 typedef struct erow {
@@ -47,6 +51,7 @@ typedef struct erow {
     int rsize;
     char *chars; 
     char *render; 
+    unsigned char *hl;
 } erow;
 
 struct editorConfig {
@@ -253,6 +258,7 @@ void editorInsertRow(int at, char *s, size_t len) {
 
     E.row[at].rsize = 0;
     E.row[at].render = NULL;
+    E.row[at].hl = NULL;
     editorUpdateRow(&E.row[at]);
 
     E.numrows ++;
@@ -263,6 +269,7 @@ void editorInsertRow(int at, char *s, size_t len) {
 void editorFreeRow(erow *row) {
     free(row->render);
     free(row->chars);
+    free(row->hl)
 }
 
 void editorDelRow(int at) {
